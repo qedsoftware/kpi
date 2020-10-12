fe8cb13e7a296d300a5a0c154d81cfe0a4ed5761
Michał Łazowik <m1@qed.ai>
2020-10-12 23:02:32 +0200

    Update user email when using remote auth

--- kobo/auth.py
+++ kobo/auth.py
@@ -3,3 +3,24 @@ from django.contrib.auth.middleware import RemoteUserMiddleware
 
 class QedAuthMiddleware(RemoteUserMiddleware):
     header = 'HTTP_X_AUTH_USERNAME'
+
+
+class QedRemoteUserAttributeMiddleware(object):
+    username_header = 'HTTP_X_AUTH_USERNAME'
+    email_header = 'HTTP_X_AUTH_EMAIL'
+
+    def process_request(self, request):
+        username = request.META.get(self.username_header, None)
+        email = request.META.get(self.email_header, None)
+
+        if hasattr(request, 'user') and request.user.is_authenticated and \
+                username and email:
+            user = request.user
+            needs_saving = False
+
+            if user.email != email:
+                user.email = email
+                needs_saving = True
+
+            if needs_saving:
+                user.save()
